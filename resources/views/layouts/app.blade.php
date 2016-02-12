5bdfa2284095ac01dc3037bb4423e0a90150ad5f
silvan <silvan@wyden.com>
2016-02-12 22:43:17 +0100

    set brand as home link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
 						<span class="icon-bar"></span>
 					</button>
 			
-		   	    	<a class="navbar-brand" href="#">
+		   	    	<a class="navbar-brand" href="/">
 				        <div class="glyphicon glyphicon-cloud"></div>
 				    </a>
 			
