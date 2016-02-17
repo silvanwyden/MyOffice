d1247d8f63712e6281b499e5b784fc690674c854
silvanwyden <silvan@wyden.com>
2016-02-17 13:29:31 +0100

    bugfix

--- app/Http/Controllers/PasspackController.php
+++ app/Http/Controllers/PasspackController.php
@@ -140,6 +140,7 @@ class PasspackController extends Controller
     	return view('passpacks.update', [
     			'categories' => $categories,
     			'category_id' => $user->passpack_category_id,
+    			'pwd' = '',
     			])->withPasspack(new Passpack());
     	 
     }
