5aee559f1b56944a243fd69df61176a45bf7d78d
silvan <silvan@wyden.com>
2016-02-19 20:51:58 +0100

    base note object

---
+++ database/migrations/2016_02_19_192132_create_notes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('category_id')->index();
+            $table->string('tag_ids');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notes');
+    }
+}
