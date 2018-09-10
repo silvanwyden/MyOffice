cee04d58be9da1efbea907d05d87870bd88772af
Silvan Wyden <silvan@wyden.com>
2018-09-10 22:32:50 +0200

    Changing navigation to fixed - Bugfix css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,7 +165,7 @@
 			</div>
 		</nav>
 
-		<div class="container" style="margin-top: 59px;">
+		<div class="container" style="margin-top: 59px; padding-left: 0px; padding-right: 0px;">
 
 			@yield('content')
 
